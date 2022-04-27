67c5ce3a5b7b8c657206a7d7ba100a4cebc74436
GabrielDeA <102760634+GabrielDeA@users.noreply.github.com>
2022-04-27 10:06:58 -0300

    Add files via upload

---
+++ Unidade5/src/Uni5Exe03.java
@@ -0,0 +1,12 @@
+public class Uni5Exe03 {
+    public static void main(String[] arStrings ) {
+        double Soma=0;
+        for (double i = 2; i <=100; i++) {
+            Soma= Soma + 1/i;
+            System.out.println(i);
+               
+        }
+ System.out.println(Soma);
+}
+    
+}
