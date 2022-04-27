957c50a8401e5dd36caa52b969f918513b3f40ef
GabrielDeA <102760634+GabrielDeA@users.noreply.github.com>
2022-04-27 10:13:22 -0300

    Add files via upload

---
+++ Unidade5/src/Un5Exe01.java
@@ -0,0 +1,14 @@
+public class Un5Exe01 {
+    public static void main(String[] argStrings) {
+        for (int i = 1; i<=20 ; i++) {
+            if (i%2==0) {
+                System.out.println(i + " é par");
+            } else {
+                System.out.println(i + " é impar");
+            }
+            
+        }
+        
+    }
+    
+}
