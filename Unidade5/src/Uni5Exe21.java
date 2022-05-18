7f94eab9c4c1c9f0249337003af9b1194c5db16a
GabrielDeA <102760634+GabrielDeA@users.noreply.github.com>
2022-05-18 10:04:36 -0300

    Add files via upload

---
+++ Unidade5/src/Uni5Exe21.java
@@ -0,0 +1,14 @@
+public class Uni5Exe21 {
+    public static void main(String[] args) {
+        int chico = 150;
+        int ze = 110;
+        int ano = 0;
+        while (chico >= ze) {
+           chico = chico + 2;
+           ze = ze + 3;
+           ano++;
+            }
+            System.out.println("Zé será maior que chico em " + ano + " anos");
+        }
+    }
+
